15c21916949325bb3c1366d547f0a4aa3902871d
Abubakar-Aijaz <147841065+ABUBAKARKHAN-Stack@users.noreply.github.com>
2024-08-07 14:56:55 +0000

    Project 2 Movie Search App Completed

--- Project 2 Movie Search App/script.js
+++ Project 2 Movie Search App/script.js
@@ -69,20 +69,24 @@ async function movieData(movieName) {
             document.querySelector('.movie-poster').src = data.Poster; // Display movie poster
             document.querySelector('.release-Year').style.display = 'block'; // Show release year
             document.querySelector('.release-Year').innerHTML = 'Release Year: ' + data.Year; // Display release year
-            document.querySelector('.release-date').innerHTML  = 'Release Date: ' + data.Released
+            document.querySelector('.more-info').style.display  = 'block'
+            document.querySelector('.rating-div').style.display  = 'flex'
+            document.querySelector('.release-date').innerHTML = 'Release Date: ' + data.Released
             document.querySelector('.genre').innerHTML = "Genre: " + data.Genre
             document.querySelector('.rating').innerHTML = data.Ratings[0].Value
-            document.querySelector('.writer').innerHTML = `Writer: "${data.Writer}"`
+            document.querySelector('.writer').innerHTML = `Writer: ${data.Writer}`
             document.querySelector('.movie-title').innerHTML = data.Title
-            document.querySelector('.movie-type').innerHTML = 'Type ' + data.Type
-            document.querySelector('.plot').innerHTML = 'Plot: ' + data.Plot
-            document.querySelector('.movie-director').innerHTML = 'Director: ' + data.Director
-            document.querySelector('.movie-actors').innerHTML = 'Actors: ' + data.Actors
+            document.querySelector('.movie-type').innerHTML = `"${data.Type.toUpperCase()}"`
+            document.querySelector('.plot').innerHTML = data.Plot
+            document.querySelector('.movie-director').innerHTML = data.Director
+            document.querySelector('.movie-actors').innerHTML = data.Actors
         } else {
             document.querySelector('.title').style.color = 'red'; // Set title color to red if movie not found
             document.querySelector('.title').innerHTML = 'Movie not found!'; // Display movie not found message
             document.querySelector('.movie-poster').src = 'https://via.placeholder.com/600x400?text=Movie%20Not%20Found%20:('; // Display placeholder image
             document.querySelector('.release-Year').style.display = 'none'; // Hide release year
+            document.querySelector('.more-info').style.display  = 'none'
+            document.querySelector('.rating-div').style.display  = 'none'
         }
     } catch (error) {
         console.error('Error fetching movie data:', error); // Log any errors that occur
