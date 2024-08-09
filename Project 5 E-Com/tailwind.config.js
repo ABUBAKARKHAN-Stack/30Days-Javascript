88f57d38a4bd732ef9a24a9ff87c25329103c475
Abubakar-Aijaz <abubakar123456654123@gmail.com>
2024-08-10 02:04:26 +0500

    Project 5

---
+++ Project 5 E-Com/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
